d762e6a3bf14d4b75702ebb0065a0716de9437bf
Jose Manuel Robles Hijano <hijano87@gmail.com>
2022-03-26 20:05:59 +0100

    feat: apply material design dynamic colors

---
+++ app/src/main/java/com/hijano/games/GamesApplication.kt
@@ -0,0 +1,11 @@
+package com.hijano.games
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class GamesApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
